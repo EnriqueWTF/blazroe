cc21f97d66ea82363b9f4657b75be5c4fa26bf33
Enrique <proenrique4@gmail.com>
2025-11-09 17:57:42 -0700

    sin funcionamiento para base de datos

---
+++ Components/Data/Articulo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace blazroe.Components.Data
+{
+    public class Articulo
+    {
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+    }
+}
---
+++ Components/Data/Factura.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace blazroe.Components.Data 
+{
+    public class Factura
+    {
+        public int Identificador { get; set; }
+
+       
+        public string NombreCliente { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+       
+        public List<Articulo> Articulos { get; set; } = new List<Articulo>();
+
+        public decimal Total => Articulos.Sum(articulo => articulo.Precio);
+    }
+}
