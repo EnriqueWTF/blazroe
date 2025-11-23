f921e9ac57d50bf0297d210a5ff9f9e2ef80b6f2
Enrique <proenrique4@gmail.com>
2025-11-23 15:37:31 -0600

    id para la factura y nombre vacio

--- Components/Data/Factura.cs
+++ Components/Data/Factura.cs
@@ -1,20 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace blazroe.Components.Data 
+namespace blazroe.Components.Data
 {
     public class Factura
     {
-        public int Identificador { get; set; }
-
-       
-        public string NombreCliente { get; set; }
-
+        public int Id { get; set; }
+        public string NombreCliente { get; set; } = string.Empty;
         public DateTime Fecha { get; set; } = DateTime.Now;
 
-       
         public List<Articulo> Articulos { get; set; } = new List<Articulo>();
-
         public decimal Total => Articulos.Sum(articulo => articulo.Precio);
     }
-}   
+}
