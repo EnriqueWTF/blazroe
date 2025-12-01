69bc6ae5ae0d8e850d1687a317be90c9c456c198
Enrique <proenrique4@gmail.com>
2025-11-30 22:37:15 -0600

    marca para saber si esta archivada

--- Components/Data/Factura.cs
+++ Components/Data/Factura.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using blazroe.Components.Data;
 
-namespace blazroe.Components.Data
+public class Factura
 {
-    public class Factura
-    {
-        public int Id { get; set; }
-        public string NombreCliente { get; set; } = string.Empty;
-        public DateTime Fecha { get; set; } = DateTime.Now;
+    public int Id { get; set; }
+    public string NombreCliente { get; set; } = string.Empty;
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    public List<Articulo> Articulos { get; set; } = new List<Articulo>();
+    public decimal Total => Articulos.Sum(articulo => articulo.Precio);
 
-        public List<Articulo> Articulos { get; set; } = new List<Articulo>();
-        public decimal Total => Articulos.Sum(articulo => articulo.Precio);
-    }
+    
+    public bool Archivada { get; set; } = false; // Por defecto no esta archivada
 }
